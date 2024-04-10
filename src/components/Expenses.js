025d8dcb4f050b190ac589dbdc56369d166ec7ac
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-04-11 03:07:05 +0530

    edit

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 
-function Expenses(){
-    const expenses = [
-        { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location: "Bangalore" },
-        { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location: "Delhi" },
-        { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location: "Hyderabad" },
-        { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location: "Mumbai" },
-    ];
+function Expenses(props){
+    // const expenses = [
+    //     { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location: "Bangalore" },
+    //     { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location: "Delhi" },
+    //     { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location: "Hyderabad" },
+    //     { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location: "Mumbai" },
+    // ];
 
     return (
         <div>
-            {expenses.map((expense) => {
+            {props.expenses.map((expense) => {
                 return (
                     <ExpenseItem
                         key={expense.id}
