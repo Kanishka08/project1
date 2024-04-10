cb51282734fc92293280a5a6d2c38f95f8360481
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-04-11 02:58:22 +0530

    splitting the components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,42 +1,20 @@
 import React from "react";
 import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
 
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-];
 
 function ExpenseItem(props) {
-    const month = months[props.date.getMonth()];
-    const year = props.date.getFullYear();
-    // const date = props.date.getDate();
-    const day = props.date.getDate().toLocaleString('en-US', {minimumIntegerDigits: 2});
-
+    
     return (
-      <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
+        <div className="expense-item">
+          <ExpenseDate calendarDate={props.date} />
+          <div className="expense-item__location">{props.location}</div>
+          <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.price}</div>
+          </div>
         </div>
-        {/* <div>{props.date.toISOString()}</div> */}
-        <div className="expense-item__location">{props.location}</div>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.price}</div>
-        </div>
-      </div>
-    );
+      );
   }
 
 
