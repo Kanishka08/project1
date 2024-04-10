f56e6d1aa03bc7606cb189b7b6637167523ce7c2
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-04-10 20:52:38 +0530

    javascript inside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,17 +2,22 @@ import React from "react";
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  return(
-    <div className="expense-item">
-    <div>15 August  2023</div>
-    <div className="expense-item__location">Delhi</div>
-    <div className="expense-item__description">
-        <h2>Book</h2>
-        <div className="expense-item__price">$10</div>
-    </div>
-  </div>
-  )
+    const expenseDate = new Date(2023, 7, 15).toISOString();
+    const expenseTitle = "Insurance";
+    const expenseAmount = 50;
+    const expenseLocation = "Bangalore"; 
   
-}
+    return (
+      <div className="expense-item">
+        <div>{expenseDate}</div>
+        <div className="expense-item__location">{expenseLocation}</div> 
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${expenseAmount}</div>
+        </div>
+      </div>
+    );
+  }
+  
 
 export default ExpenseItem;
