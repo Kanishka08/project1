b7644f5ffcbe1ec3100c707d36d1a95c2567db26
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-04-10 21:47:31 +0530

    Formatting Date - Giving Calendar Look

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,35 @@
 import React from "react";
 import './ExpenseItem.css';
 
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
 function ExpenseItem(props) {
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    // const date = props.date.getDate();
+    const day = props.date.getDate().toLocaleString('en-US', {minimumIntegerDigits: 2});
+
     return (
       <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+        {/* <div>{props.date.toISOString()}</div> */}
         <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
           <h2>{props.title}</h2>
